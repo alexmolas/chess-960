b4625ae36698bba918d076adc14ecf0ab4516f06
Alex Molas <a.molasmartin@stuart.com>
2023-01-18 16:38:55 +0100

    Fix typos with download data script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -10,15 +10,18 @@ BASE_PATH = "https://database.lichess.org/chess960/lichess_db_chess960_rated"
 
 def download_month(year: int, month: int, destination_path):
     path = f"{BASE_PATH}_{year}-{month:02}.pgn.zst"
-    logger.info(f"Downloading {path} to {destination_path}")
-    urllib.request.urlretrieve(path, destination_path)
-    os.system(f"unzstd -f {destination_path}")
-    os.remove(destination_path)
+    try:
+        logger.info(f"Downloading {path} to {destination_path}")
+        urllib.request.urlretrieve(path, destination_path)
+        os.system(f"unzstd -f {destination_path}")
+        os.remove(destination_path)
+    except:
+        logger.info(f"{path} doesn't exist")
 
 
 def download_all_data():
-    years = range(2022, 2023)
-    months = range(9, 13)
+    years = range(2013, 2030)
+    months = range(1, 13)
     for year in years:
         for month in months:
             destination_path = f"data/raw/{year}-{month:02}.pgn.zst"
